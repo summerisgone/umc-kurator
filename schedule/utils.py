c2951d7a14646ae79181c463a73088f6f01e473f
Ivan Gromov <ivan.gromov@redsolution.ru>
2012-09-29 03:08:36 +0600

    Добавил преподавателей в меню

--- schedule/utils.py
+++ schedule/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from difflib import get_close_matches
 from schedule import enums
+import re
 
 def get_position_fuzzy(original):
     matches = get_close_matches(original, [item[1] for item in enums.LISTENER_POSITIONS])
@@ -8,3 +9,8 @@ def get_position_fuzzy(original):
         return matches[0]
     else:
         return enums.LISTENER_POSITIONS[-1][1]
+
+def get_organization_type(original):
+    for org_type in enums.ORGANIZATION_TYPES:
+        if re.search('%s' % org_type[1], original):
+            return org_type[1]
